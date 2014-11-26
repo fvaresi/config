09eac44639bd8d960b841e1a5a74a0136b4ced78
Fernando Varesi <fvaresi@gmail.com>
2014-11-26 11:56:31 -0200

    Upgrading to cider 0.8.1.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [
-                  ;; [cider/cider-nrepl "0.7.0"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  ;;[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.8.1"]
                   ]}}
