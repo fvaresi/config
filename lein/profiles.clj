cb6dd9ad54cf7102643425e6894461d137f807aa
Fernando Varesi <fvaresi@gmail.com>
2015-07-03 15:15:19 -0300

    Updating cider-nrepl version.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-droid "0.4.0-alpha2"]
                   [lein-try "0.4.3"]
                   ]}
