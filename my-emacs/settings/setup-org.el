64139bef4bfc45eed95faaecfa7f239da2d73fce
Fernando Varesi <fvaresi@gmail.com>
2015-07-11 23:07:45 -0300

    setup capture for journal

--- my-emacs/settings/setup-org.el
+++ my-emacs/settings/setup-org.el
@@ -1,9 +1,18 @@
 ;; export org files to confluence
 ;;(require 'ox-confluence)
 
+(setq standup-journal "~/autocomm/docs/standup-journal.org")
+
+(setq org-agenda-files `(,standup-journal))
+
 ;; org capture
 (setq org-directory "~/org/")
 (setq org-mobile-directory "~/Dropbox/org-mobile")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(setq org-capture-templates
+      `(("s" "Standup" entry
+	 (file+datetree+prompt ,standup-journal)
+	 "* %T - %?" :kill-buffer t)))
 
 (provide 'setup-org)
