c71577b0a0f1d47496902b8ee37c3e36cc2049b3
Fernando Varesi <fvaresi@gmail.com>
2015-08-10 12:06:55 -0300

    Setting up orgmobile and new task progress system

--- my-emacs/settings/setup-org.el
+++ my-emacs/settings/setup-org.el
@@ -3,10 +3,10 @@
 
 (setq standup-journal "~/autocomm/docs/standup-journal.org")
 
-(setq org-agenda-files `("~/autocomm/docs/standup.org" ,standup-journal))
+(setq org-agenda-files `(,standup-journal))
 
 ;; org capture
-(setq org-directory "~/org/")
+(setq org-directory "~/org")
 (setq org-mobile-directory "~/Dropbox/org-mobile")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
@@ -17,7 +17,7 @@
 	 :kill-buffer t)
 
 	("b" "Bookmark" entry
-	 (file+headline org-default-notes-file) "Bookmarks"
+	 (file+headline org-default-notes-file "Bookmarks")
 	 "* %?\n:CREATED: %U\n:END:\n\n"
 	 :empty-lines 1)
 
@@ -25,5 +25,18 @@
 	 (file+datetree+prompt ,standup-journal)
 	 "* %T - %?"
 	 :kill-buffer t)))
+
+;; org clock
+(setq org-clock-out-remove-zero-time-clocks t)
+
+;; org log
+(setq org-log-into-drawer t)
+
+;; org mobile
+(setq org-mobile-files `(org-agenda-files ,org-default-notes-file))
+(setq org-mobile-inbox-for-pull "~/org/from-mobile.org")
+
+;; org todo
+(setq org-todo-keywords `((sequence "TODO(t)" "IN_PROGRESS(p)" "|" "DONE(d)")))
 
 (provide 'setup-org)
