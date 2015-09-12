4be1c04a70f8075b97d8a868017382f95748862a
Fernando Varesi <fvaresi@gmail.com>
2015-09-12 12:12:25 -0300

    Removing unrequired lein dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,7 @@
-{:user {:dependencies [[slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+{:user {:dependencies []
+        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl "1.2.0-SNAPSHOT"]
-                  [lein-droid "0.4.0-alpha4"]
-                  [lein-try "0.4.3"]
-                  ]}
+                  [lein-droid "0.4.0-alpha4"]]}
 
  :android-common {:dependencies [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
                   :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
