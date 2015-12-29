c530ade4576bcfe43ac175c803272e0d8fa988ca
Fernando Varesi <fvaresi@gmail.com>
2015-12-29 15:59:08 -0300

    Pinned cider and clj-refactor to melpa stable
    Some projects are still using Clojure 1.6 so we can't use the latest
    version of cider.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,8 +3,8 @@
                   [lein-try "0.4.3"]]}
 
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-        :plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]}
+        :plugins [[refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.10.0"]]}
 
  :android-common {:dependencies [[cider/cider-nrepl "0.10.0"]]
                   :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
