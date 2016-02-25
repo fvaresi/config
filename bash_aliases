04e8e09bd66dd204959c992aadc2d2e92a2425f9
Fernando Varesi <fvaresi@gmail.com>
2016-02-25 14:09:46 -0300

    Multiple changes

--- bash_aliases
+++ bash_aliases
@@ -1,5 +1,4 @@
 alias info='info --vi-keys'
-alias backup_rename="rename -v 's/(IMG|VID)_(\d{8})_(\d{6})\.(jpg|3gp)/$2$3\.$4/' *"
 alias svndiff='svn diff --diff-cmd=svn-diff-meld'
 alias gitdiff='git diff --ext-diff'
 alias memcache_stat='watch "echo stats | nc 127.0.0.1 11211"'
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,9 +4,9 @@
 
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[refactor-nrepl "1.1.0"]
-                  [cider/cider-nrepl "0.10.1"]]}
+                  [cider/cider-nrepl "0.10.2"]]}
 
- :android-common {:dependencies [[cider/cider-nrepl "0.10.1"]]
+ :android-common {:dependencies [[cider/cider-nrepl "0.10.2"]]
                   :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
                                              "cider.nrepl" "cider-nrepl.plugin"]
                             :sdk-path "/home/fvaresi/opt/android-sdk-linux"}}}
