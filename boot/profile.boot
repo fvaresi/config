db0103208efea4365ba55a622801116381e1699b
Fernando Varesi <fvaresi@gmail.com>
2016-03-29 16:56:18 -0300

    Upgrading cider to melpa (0.11.*)

--- boot/profile.boot
+++ boot/profile.boot
@@ -1,7 +1,7 @@
 (require 'boot.repl)
 
 (swap! boot.repl/*default-dependencies*
-       concat '[[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+       concat '[[cider/cider-nrepl "0.11.0"]
                 [refactor-nrepl "1.2.0-SNAPSHOT"]])
 
 (swap! boot.repl/*default-middleware*
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,12 +1,1 @@
-{:user {:dependencies []
-        :plugins [[lein-droid "0.4.0-alpha4"]
-                  [lein-try "0.4.3"]]}
-
- :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-        :plugins [[refactor-nrepl "1.1.0"]
-                  [cider/cider-nrepl "0.10.2"]]}
-
- :android-common {:dependencies [[cider/cider-nrepl "0.10.2"]]
-                  :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
-                                             "cider.nrepl" "cider-nrepl.plugin"]
-                            :sdk-path "/home/fvaresi/opt/android-sdk-linux"}}}
+{:user {:plugins [[lein-try "0.4.3"]]}}
